f14125e84ca328bbbc77b8fb0b8582b190881af2
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-23 19:42:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,17 @@ class Shoe
     @brand 
   end 
   
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material
   attr_reader :brand 
   
+  def cobble 
+    puts "The shoe has been repaired" 
+  end 
+  
+  def condition(condition)
+    condition = new 
+  end 
+    
   
   
   
